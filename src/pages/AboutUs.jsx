35414b7f3ca388296384df26d9227c7cb1838fbd
violet978 <villette@live.com>
2022-12-01 22:06:45 -0700

    Styling about page

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -2,32 +2,33 @@ import React from 'react'
 import { Helmet } from 'react-helmet-async';
 
 const AboutUs = () => {
+
   return (    
     <div className="about">
     <Helmet>
     <title>About Us</title>
     <meta name='description' content='About Rvisions' />
     </Helmet>
-<img src="./images/suitHollowgraph.jpeg" alt="Hands holding silhouettes of people"></img>
-        <h2>R Visions Medical Staffing, LLC <br/>
-Leading the Industry with Innovative Travel Opportunities</h2>
 
-<p>At R Visions, we offer a diverse and growing array of both temporary and permanent placement opportunities throughout the country. We provide these services through direct relationships with hospitals, providers and our partnerships with the top Vendor/Manager companies in the nation.<br/>
-<br/>
-R Visions was founded with the mission of assisting the highest quality healthcare professionals in meeting their goals in the travel industry along with providing unparalleled customer service to the facilities and vendor managers we work with. Our team is dedicated to meeting the unique needs of every traveler and client we serve. Our professional recruiters individually have over 20 years‘ experience in the travel industry and provide exceptional service and support. <br/>
-<br/>
+    <img src="./images/suitHollowgraph.jpeg" alt="Hands holding silhouettes of people"></img>
 
-Innovation means an ongoing and concerted effort at providing the most efficient, timely, friendly, and comprehensive representation in the industry, for both our health care professionals, and the vendors and facilities that partner with us. <br/>
-<br/>
-A Proven Reputable Company for 15 years Founded and Operated by Healthcare Professionals The Highest Quality Providers Flexible to Meet Your Needs Committed to Your Satisfaction Offering Top Quality Assignments in Rural Community Hospitals, Many of the Nation’s Largest Healthcare Facilities, and Long Term Care Facilities Helping to Promote Transparency in Our Industry and Recognized by Our Travelers as the Agency They Can Trust.<br/>
-<br/>
-For more information on our services contact us.
-info@rvisions.org <br/>
-<br/>
+        <h2>R Visions Medical Staffing, LLC</h2>
+        <h3><em>Leading the Industry with Innovative Travel Opportunities</em></h3>
 
-Thank you for visiting our web site home, we look forward to working with you!
+          <p>At R Visions, we offer a diverse and growing array of both temporary and permanent placement opportunities throughout the country. We provide these services through direct relationships with hospitals, providers and our partnerships with the top Vendor/Manager companies in the nation.<br/>
+          <br/>
+          R Visions was founded with the mission of assisting the highest quality healthcare professionals in meeting their goals in the travel industry along with providing unparalleled customer service to the facilities and vendor managers we work with. Our team is dedicated to meeting the unique needs of every traveler and client we serve. Our professional recruiters individually have over 20 years‘ experience in the travel industry and provide exceptional service and support. <br/>
+          <br/>
+          Innovation means an ongoing and concerted effort at providing the most efficient, timely, friendly, and comprehensive representation in the industry, for both our health care professionals, and the vendors and facilities that partner with us. <br/>
+          <br/>
+          A Proven Reputable Company for 15 years Founded and Operated by Healthcare Professionals The Highest Quality Providers Flexible to Meet Your Needs Committed to Your Satisfaction Offering Top Quality Assignments in Rural Community Hospitals, Many of the Nation’s Largest Healthcare Facilities, and Long Term Care Facilities Helping to Promote Transparency in Our Industry and Recognized by Our Travelers as the Agency They Can Trust.<br/>
+          <br/>
+          For more information on our services contact us.
+          info@rvisions.org <br/>
+          <br/>
 
-</p>
+          Thank you for visiting our web site home, we look forward to working with you!
+          </p>
     
     </div>
   )
